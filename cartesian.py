0ba2e1ec0b502d40c57de38e8eb06368ce4db057
ugovaretto <ugovaretto@gmail.com>
2024-04-25 10:11:47 +0800

    Added functions for midpoint and gradient, cleaned up

--- cartesian.py
+++ cartesian.py
@@ -5,94 +5,74 @@ type Position = tuple[int, int]
 type Grid = list[list[str]]
 
 # Return random 2D coordinates.
+
+
 def rand_coord(max_coord: int) -> tuple[int, int]:
     return (randint(-max_coord, max_coord), randint(-max_coord, max_coord))
 
 # Compute the square of the distance between two points (Pythagora's theorem).
+
+
 def distance_sq(a: Position, b: Position) -> float:
     return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
 
 # Compute the distamce between two points.
+
+
 def distance(a: tuple[int, int], b: tuple[int, int]) -> float:
     return sqrt(distance_sq(a, b))
-
 
 # Given octant code from 1 to 8 and a and b triangle sides
 # return the x and y coordinates sucn that x*x + y*y == a*a + b*b
 # with the right signs to fit in the quandrant.
+
+
 def ab_to_xy(ab: tuple[int, int], octant: int) -> tuple[int, int]:
     a, b = ab
-    
-    if octant == 1: # 0-45 degrees
+
+    if octant == 1:  # 0-45 degrees
         if a > b:
             return (a, b)
         else:
             return (b, a)
-    elif octant == 2: # 45-90 degrees
+    elif octant == 2:  # 45-90 degrees
         if b > a:
             return (a, b)
         else:
             return (b, a)
-    elif octant == 3: # 90 - 135 degrees
+    elif octant == 3:  # 90 - 135 degrees
         if b > a:
-            return (b, -a)
-        else:
             return (-a, b)
-    elif octant == 4: # 135 - 180 degrees
+        else:
+            return (-b, a)
+    elif octant == 4:  # 135 - 180 degrees
         if a > b:
             return (-a, b)
         else:
-            return (b, -a)
-    elif octant == 5: # 180 - 225 degrees 
+            return (-b, a)
+    elif octant == 5:  # 180 - 225 degrees
         if a > b:
             return (-a, -b)
         else:
             return (-b, -a)
-    elif octant == 6: # 225 - 270 degrees
+    elif octant == 6:  # 225 - 270 degrees
         if b > a:
             return (-a, -b)
         else:
             return (-b, -a)
-    elif octant == 7: # 270 - 315 degrees
+    elif octant == 7:  # 270 - 315 degrees
         if b > a:
             return (a, -b)
         else:
             return (-b, a)
-    elif octant == 8: # 270 - 360 degrees
+    elif octant == 8:  # 270 - 360 degrees
         if a > b:
             return (a, -b)
         else:
             return (-b, a)
 
-    # We never get here, just to make the warning about returning from all code paths go away
-    return (0, 0)
-
-
-# Geive two positions return the direction and number of units to move towards the destination
-def reverse_grad(p1: Position, p2: Position) -> tuple[int, int]:
-    dx = p2[0] - p1[0]
-    dy = p2[1] - p2[1]
-    d = int(distance(p1,p2))
-    if dx == 0:
-        dx = 1
-    if dy == 0:
-        dy = 1
-    if dx > dy and dy >= 0  and dx >= 0:
-        return (d, 1)
-    elif dy > dx and dx > 0 and dy > 0:
-        return (d, 2)
-    elif dy >= abs(dx) and dx < 0:
-        return (d, 3)
-    elif dy <= abs(dx) and dy > 0 and dx < 0:
-        return (d, 4)
-    elif dy < 0 and dx < 0 and abs(dx) >= abs(dy):
-        return (d, 5)
-    elif dy < 0 and dx < 0 and abs(dy) >= abs(dx):
-        return (d, 6)
-    elif dy < 0 and dx > 0 and abs(dy) >= dx:
-        return (d, 7)
-    elif dy < 0 and dx > 0 and abs(dx) >= abs(dy):
-        return (d, 8)
+    # We never get here, just to make the warning about returning from all code
+    # paths go away
     return (0, 0)
 
 
@@ -103,15 +83,28 @@ def add_points(p1: Position, p2: Position) -> Position:
 def sub_points(p1: Position, p2: Position) -> Position:
     return (p1[0] - p2[0], p1[1] - p2[1])
 
-def scale(p: Position, minp: Position, maxp: Position, outmin: Position, outmax: Position) -> Position:
+
+def grad(start: Position, end: Position) -> float:
+    grad = (end[1] - start[1]) / (end[0] - start[0])
+    return grad
+
+
+def midpoint(p1: Position, p2: Position) -> Position:
+    mid_point = ((p1[0] + p2[0]) / 2.0, (p1[1] + p2[1]) / 2.0)
+    return mid_point
+
+
+def scale(p: Position, minp: Position, maxp: Position,
+          outmin: Position, outmax: Position) -> Position:
     p2 = sub_points(p, minp)
     d = sub_points(maxp, minp)
-    s =  (float(p2[0]) / float(d[0]), float(p2[1])/  float(d[1]))
+    s = (float(p2[0]) / float(d[0]), float(p2[1]) / float(d[1]))
     dout = sub_points(outmax, outmin)
     return (int(s[0]*dout[0]), int(s[1]*dout[1]))
 
+
 def gen_grid(h: int, v: int) -> Grid:
-    g:list[list[str]] = []
+    g: list[list[str]] = []
     for _ in range(h):
         row = []
         for _ in range(v):
@@ -119,9 +112,11 @@ def gen_grid(h: int, v: int) -> Grid:
         g.append(row)
     return g
 
-def set_grid_elem(row: int, col: int, elem: str, grid: Grid) -> Grid:
-    grid[row][col] = elem
+
+def set_grid_elem(x: int, y: int, elem: str, grid: Grid) -> Grid:
+    grid[y][x] = elem
     return grid
+
 
 def print_grid(grid: Grid) -> None:
     for r in grid:
--- fancy_game2.py
+++ fancy_game2.py
@@ -1,47 +1,59 @@
-# Given the initial position of a number of players (no limit on the number of players)
-# and a destination position, at each turn each player can specify the horizontal and
-# vertical movements with the goal to reach the destination.
-# If after a specified number of turns no player has reached the destination the player
-# closes to the destination wins.
+# Given the initial position of a number of players (no limit on the number of
+# players) and a destination position, at each turn each player can specify the
+# horizontal and vertical movements with the goal to reach the destination.
+# If after a specified number of turns no player has reached the destination
+# the player closes to the destination wins.
 
 # Add local direactory to PYTHONPATH to be able to load code from other files.
+from cartesian import Position, distance, rand_coord, scale, \
+    ab_to_xy, distance_sq, print_grid, gen_grid, set_grid_elem
+from pythagora import generate_triples, \
+    triples_dict, closest_triple
+from math import inf
 import sys
-sys.path.insert(0,'.')
+sys.path.insert(0, '.')
 
-from math import sqrt, inf
-from random import randint
-from cartesian import * 
-from pythagora import *
 
 # Types,
 type GameState = dict[str, tuple[int, int]]
-type PlayerInput = tuple[int, int] # units and direction
+type PlayerInput = tuple[int, int]  # units and direction
 
-# Check if a player has won by checking if the current position is same as destination.
-def won(state: dict[str, tuple[int, int]], destination: tuple[int, int], min_dist: float = 0.0) -> str:
+# Check if a player has won by checking if the current position is same as
+# destination.
+
+
+def won(state: dict[str, tuple[int, int]], destination: tuple[int, int],
+        min_dist: float = 0.0) -> str:
     for name, position in state.items():
         if distance(destination, position) <= min_dist:
             return name
     return ""
 
 # Initialise game state with random positions for players and destination.
-def init_game_state(players:list[str], max_coord: int) -> GameState:
-    state:GameState = dict()
+
+
+def init_game_state(players: list[str], max_coord: int) -> GameState:
+    state: GameState = dict()
     for p in players:
         state[p] = rand_coord(max_coord)
     return state
 
-# Print current state: Players' positions, destination and number of turns left.
-def print_state(state: GameState, destination: Position, turns_left: int) -> None:
+# Print current state: Players' positions, destination and number of
+# turns left.
+
+
+def print_state(state: GameState, destination: Position,
+                turns_left: int) -> None:
     print()
     print("=" * 30)
     print(state)
     print("Turns left: " + str(turns_left))
     print(f"Destination: {destination}")
-    
+
     for (player, position) in state.items():
         print()
-        print(f"{player}'s distance from destination: {int(distance(position, destination))}")
+        print(f"{player}'s distance from destination: " +
+              f"{int(distance(position, destination))}")
         dydx = (destination[1] - position[1]) / (destination[0]-position[0])
         print(f"{player}'s gradient with destination: {dydx:.2f}")
         # (units, dir) = reverse_grad(position, destination)
@@ -49,41 +61,53 @@ def print_state(state: GameState, destination: Position, turns_left: int) -> Non
         for (player2, position2) in state.items():
             if player2 == player:
                 continue
-            mp = ((position[0] + position2[0])/2.0, (position[1] + position2[1])/2.0)
-            print(f"{player} - {player2} midpoint = ({mp[0]:.2f}, {mp[1]:.2f})")
+            mp = ((position[0] + position2[0])/2.0,
+                  (position[1] + position2[1])/2.0)
+            print(
+                f"{player} - {player2} midpoint = ({mp[0]:.2f}, {mp[1]:.2f})")
     print("=" * 30)
 
 # Validate input also verifying that the entered numbers are a pefect triple.
-def check_input(line:str) -> bool:
+
+
+def check_input(line: str) -> bool:
     if len(line) == 0:
         return False
-    l = str.split(line)
-    if len(l) != 2:
+    sl = str.split(line)
+    if len(sl) != 2:
         return False
-    l = list(map(int, str.split(line)))
-    if len(l) != 2:
+    sl = list(map(int, str.split(line)))
+    if len(sl) != 2:
         return False
-    if l[1] < 1 or l[1] > 8:
+    if sl[1] < 1 or sl[1] > 8:
         return False
-    if l[0] <= 0:
+    if sl[0] <= 0:
         return False
     return True
 
 # Return player input as (x,y) coordinate.
+
+
 def get_player_input(player: str) -> PlayerInput:
-    line = input(f"{player}>\nEnter units of movements and directions(1-8) as two numbers separated with space': ")
+    line = input(
+        f"{player}>\nEnter units of movements and directions(1-8)" +
+        "as two numbers separated with space': ")
     if not check_input(line):
         print("Input error, please enter two numbers separated by space")
         get_player_input(player)
-    l = list(map(int, str.split(line)))
-    return (l[0], l[1])
+    sl = list(map(int, str.split(line)))
+    return (sl[0], sl[1])
 
 # Main function implementing the game logic.
+
+
 def main(destination: Position, max_coord: int, max_number_of_turns: int):
-    X = 40 # column
-    Y = 40 # row
+    X = 40  # column
+    Y = 40  # row
+    MAX_COORD = max_coord
     grid = gen_grid(X, Y)
-    sd = scale(destination, (-800, -800), (800, 800), (0,0), (40, 40))
+    sd = scale(destination, (-MAX_COORD, -MAX_COORD),
+               (MAX_COORD, MAX_COORD), (0, 0), (Y, X))
     grid = set_grid_elem(- sd[0], sd[1], 'D', grid)
     print("Generating Pythagorean triples...")
     triples = generate_triples(127, 2000)
@@ -95,20 +119,22 @@ def main(destination: Position, max_coord: int, max_number_of_turns: int):
         for (player, position) in state.items():
             (units, direction) = get_player_input(player)
             # Retrieve triple with hypotenuse having length closest to units
-            abc = closest_triple(triples_dict(triples), units) 
+            abc = closest_triple(triples_dict(triples), units)
             print(f"Triple: {abc[0]}, {abc[1]}, {abc[2]}")
             # Map triangle sides to x,y coordinates depending on direction
             (dx, dy) = ab_to_xy((abc[0], abc[1]), direction)
             print(f"Movement: ({dx}, {dy})")
-            sp = scale(position, (-800, -800), (800, 800), (0,0), (40, 40))
-            grid = set_grid_elem(Y - sp[0], sp[1], '.', grid)
+            sp = scale(position, (-MAX_COORD, -MAX_COORD),
+                       (MAX_COORD, MAX_COORD), (0, 0), (Y, X))
+            grid = set_grid_elem(sp[0], Y - sp[1], '.', grid)
             # Move player
             state[player] = (position[0] + dx, position[1] + dy)
         turns_left -= 1
         for (player, position) in state.items():
-            sp = scale(position, (-800, -800), (800, 800), (0,0), (40, 40))
-            grid = set_grid_elem(Y - sp[0], sp[1], player[-1], grid)
-        
+            sp = scale(position, (-MAX_COORD, -MAX_COORD),
+                       (MAX_COORD, MAX_COORD), (0, 0), (Y, X))
+            grid = set_grid_elem(sp[0], Y - sp[1], player[-1], grid)
+
         print_grid(grid)
     player_won = won(state, destination)
     if player_won != "":
@@ -121,9 +147,10 @@ def main(destination: Position, max_coord: int, max_number_of_turns: int):
                 winner = player
         print("Player " + winner + " WON")
 
+
 # Entry point when invoked on the command line.
 if __name__ == "__main__":
-    max_coord = 800 # from -800 to +800 in x and y
+    max_coord = 800  # from -800 to +800 in x and y
     max_number_of_turns = 10
     destination = rand_coord(max_coord)
     main(destination, max_coord, max_number_of_turns)
