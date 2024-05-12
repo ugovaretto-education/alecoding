e98cb1bc3debbf6de8abb0f3076bcede0241d2ac
Ugo <ugovaretto@gmail.com>
2024-05-12 11:55:58 +0800

    ...

--- tutorial/lab5.py
+++ tutorial/lab5.py
@@ -1,5 +1,89 @@
-# iterators
+# iterators & exceptions
+
+# Exceptions are signals emitted by prefixing
+# the signal name with the 'raise' statement
+# and can be caught by wrapping code into
+# a try catch block where the code inside the
+# catch section is executed only if the code
+# in the try section emits an exception
+
+# raise exeption
+def throw_exception():
+    print("throw_exception() raising exception of type EnvironmentError")
+    raise EnvironmentError
 
 
-class Iterator:
-    __
+def try_catch():
+    try:
+        print("Calling function 'throw_exception'...")
+        throw_exception()
+        print("We never get here!!")  # this line is never reached
+        # because the function throw_exception
+        # raises an exception and therefore
+        # the code jumps to the 'except' statement
+    except EnvironmentError:
+        print("Exception of type 'EnvironmentError caught")
+    except Exception:
+        print("Some other exception caught")
+
+
+# Iterate over random access array (i.e. something that is indexable)
+# exposing it as a sequence
+class RandomAccessIterator:
+    def __init__(self, array):
+        # pointer to random access array
+        self._array_ref = array
+        # start index
+        self._index = 0
+        # record size
+        self._length = len(array)
+
+    # return reference to instance being created
+    # when clent code invokes 'iter(object)' this
+    # method is invoked
+    def __iter__(self):
+        return self
+
+    # invoked by client code to retrieve next element
+    # in sequence.
+    def __next__(self):
+        if self._index < self._length:
+            item = self._array_ref[self._index]
+            self._index += 1
+            return item
+        else:
+            # attempt to access an element beyond the end
+            # of the array
+            raise StopIteration
+
+
+# iterate using 'for' which will automatically invoke '__iter__'
+# and '__next__'
+def for_iteration():
+    alist = [10, 9, 8, 7]
+    it = iter(RandomAccessIterator(alist))
+    # 'for' calls 'iter' on 'it1' and then at each
+    # iteration invokes the 'next' method to retrieve
+    # the next element
+    print("Iterating over list using 'for'")
+    for element in it:
+        print(element)
+
+
+# iterate using 'while' and explicitly construct the iterator and
+# invoke '__next__' to iterate over random access sequence
+def while_next_iteration():
+    alist = [10, 9, 8, 7]
+    it = RandomAccessIterator(alist).__iter__()
+    print()
+    print("Iterating over list using 'while' and '__next__'")
+    try:
+        while True:
+            element = it.__next__()
+            print(element)
+    except StopIteration:
+        print("Iteration completed")
+
+
+# once the iteration is completed the iteration
+# object cannot be used anymore
